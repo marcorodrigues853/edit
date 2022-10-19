2bd1a45ba96f7a5cf31a7c0b90cbb03adba6ce38
Marco Rodrigues <marcoaurelio853@gmail.com>
2022-10-19 21:53:58 +0100

    new js class

---
+++ JS MODULE/js/app.js
@@ -0,0 +1,23 @@
+const name='Marco';
+let age = 12;
+const married=true;
+
+
+
+const hobbies = {
+  swimming:{
+    name: 'swimming',
+    swimmingSpeed:2,
+    local: 'home'
+  }
+}
+
+
+console.log(`O ${name} tem ${age} e tem como hobbies ${hobbies.swimming.name}` )
+
+
+
+
+const animal = name && married;
+  console.log(name && age);
+  console.log(animal);
