7a475564dd35c46f0dd9cb3396ca3ee821b50af2
Marco Rodrigues <marcoaurelio853@gmail.com>
2022-11-17 08:50:41 +0000

    fix layout and  multiple modal

--- JS MODULE/Exercicio Final JS/app.js
+++ JS MODULE/Exercicio Final JS/app.js
@@ -1,7 +1,7 @@
 console.log('js app ok')
 const sectionGalleryDom = document.createElement('section')
 
-sectionGalleryDom.classList.add('gallery', 'grid', 'grid-col--2')
+sectionGalleryDom.classList.add('gallery', 'grid')
 
 // apeend to body
 document.body.appendChild(sectionGalleryDom)
@@ -17,21 +17,6 @@ displayInfo.classList.add('display-info')
 // append to section
 sectionGalleryDom.appendChild(galleryListDom)
 sectionGalleryDom.appendChild(displayInfo)
-
-// append inside gallery list
-// const galleryItem = document.createElement('div')
-// galleryItem.classList.add('gallery-item')
-// galleryListDom.append(galleryItem)
-
-// const imageDom = document.createElement('img')
-// imageDom.src = 'http://placeimg.com/640/480/fashion'
-// imageDom.classList.add('image-thunbaill')
-// imageDom.addEventListener('click', () => {
-//   console.log('batatinhas')
-// })
-
-// // append iinside galleryItem
-// galleryItem.append(imageDom)
 
 // display inside
 const nameDom = document.createElement('div')
@@ -50,6 +35,9 @@ const genreDom = document.createElement('div')
 genreDom.classList.add('genre')
 genreDom.innerText = 'genre'
 
+const imageBigDom = document.createElement('img')
+imageBigDom.classList.add('imageBig')
+
 const cancelButton = document.createElement('button')
 cancelButton.innerText = 'back'
 cancelButton.onclick = () => {
@@ -64,20 +52,25 @@ cancelButton.onclick = () => {
   galleryList.classList.add('grid-col--4')
 }
 
-displayInfo.append(nameDom, ratingDom, launchDateDom, genreDom, cancelButton)
+displayInfo.append(
+  imageBigDom,
+  nameDom,
+  ratingDom,
+  launchDateDom,
+  genreDom,
+  cancelButton,
+)
+displayInfo.style.display = 'none'
 
 // need to receive info from  API
-
 function renderOpenDisplayInfo(id) {}
 function renderCloseDisplayInfo() {}
 
 function renderColumnsNumber(columnsNumber) {}
 
 //?  API section
-
 const url = 'https://api.jikan.moe/v4/top/anime'
 
-let postais = undefined
 async function getAnime() {
   const request = await fetch(url)
   const response = await request.json()
@@ -103,23 +96,46 @@ function renderImagesCards(animes) {
     const imageDom = document.createElement('img')
     imageDom.src = anime.images.jpg.image_url
     imageDom.classList.add('image-thunbaill')
+
     imageDom.addEventListener('click', () => {
-      // console.log('batatinhas', index, anime.title)
-      nameDom.innerText = anime.title
-      ratingDom.innerText = anime.rating
-      launchDateDom.innerText = anime.aired.from
-      // genreDom.innerText = anime.genres[0]
       const gallery = document.getElementsByClassName('gallery')[0]
       const galleryList = document.getElementsByClassName('gallery-list')[0]
       const displayInfo = document.getElementsByClassName('display-info')[0]
+
+      imageBigDom.src = anime.images.jpg.image_url
+      imageBigDom.addEventListener('click', () => {
+        const modal = document.getElementsByClassName('modal')[0]
+        if (modal) modal.remove()
+
+        const imageDom2 = document.createElement('img')
+        imageDom2.src = anime.images.jpg.image_url
+        imageDom2.classList.add('imageBig')
+
+        const modalDom = document.createElement('div')
+        modalDom.classList.add('modal')
+        modalDom.append(imageDom2)
+        document.body.append(modalDom)
+
+        const buttonDom = document.createElement('button')
+        buttonDom.innerText = 'close'
+        buttonDom.addEventListener('click', () => {
+          modalDom.remove()
+        })
+
+        modalDom.append(buttonDom)
+      })
+
+      nameDom.innerText = anime.title
+      ratingDom.innerText = anime.rating
+      launchDateDom.innerText = anime.aired.from
+
       displayInfo.style.display = 'block'
       gallery.classList.add('grid-col--2')
-      galleryList.classList.add('grid-col--4')
+      galleryList.classList.add('grid-col--2')
     })
 
     galleryItem.append(imageDom)
-    // anime.
-    galleryList.classList.remove('grid-col--4')
-    galleryList.classList.add('grid-col--2')
+    galleryList.classList.remove('grid-col--2')
+    galleryList.classList.add('grid-col--4')
   })
 }
